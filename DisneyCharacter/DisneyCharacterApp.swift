0121c2a9c98a83235728a978d586a0e996267c65
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-04-22 18:42:10 -0400

    Initial Commit

---
+++ DisneyCharacter/DisneyCharacterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyCharacterApp.swift
+//  DisneyCharacter
+//
+//  Created by Kiho Okazawa on 2023-04-22.
+//
+
+import SwiftUI
+
+@main
+struct DisneyCharacterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
